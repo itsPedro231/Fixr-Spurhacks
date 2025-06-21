f1c526caf3e2c438842d7fb169147b93155c217b
itsPedro <phfaraco@gmail.com>
2025-06-21 12:39:42 -0400

    Create message posting and thread runnning methods

--- backend/utils/openAIUtils.py
+++ backend/utils/openAIUtils.py
@@ -1,3 +1,17 @@
-
-postMessage():
-    pass
+ASSISTANT_ID=
+def postUserMessage(threadID, content):
+    res = await requests.post("https://api.openai.com/v1/threads/$threadID/messages", headers={"Content-Type": "application/json",
+        "Authorization": "Bearer ${dotenv.env["OPENAI_API_KEY"]}",
+        "OpenAI-Beta": "assistants=v2"},
+json = {"role": "user", "content": content}
+)
+def runThread():
+    res = await requests.post(
+        "https://api.openai.com/v1/threads/$threadID/runs",
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {dotenv.env['OPENAI_API_KEY']}",
+            "OpenAI-Beta": "assistants=v2",
+        },
+        json={"assistant_id": assistantID},
+    )
