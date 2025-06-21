75a43727c691d14f6e4001286be13784852373ea
Aldin Fazlic <aldinfazlic8@gmail.com>
2025-06-21 12:50:46 -0400

    add sendMessage() in routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from utils.geminiHandler import processImage
-
+from utils.openAIUtils.py import postMessage
 
 router = APIRouter()
 
@@ -13,4 +13,9 @@ async def root():
 @router.get("/upload-image")
 async def uploadImage():
     processImage()
+    return {"message": "Hello World"}
+
+@router.get("/send-message-gpt")
+async def sendMessage():
+    postMessage()
     return {"message": "Hello World"}
